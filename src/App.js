8a61633eaff7e58e60f4949f5e7d08dcaa82565a
Muskan Gupta <mg8303655@gmail.com>
2023-02-06 14:01:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Hello from './Components/Navbar';
+ import Home from './Components/Home';
+
+function App() {
+
+  return (
+    <div>
+        <Hello/>
+        <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Header.css";
+function Header() {
+  return (
+    <nav>Get Ready To Complete Our Goals</nav>
+  );
+}
+
+export default Header
